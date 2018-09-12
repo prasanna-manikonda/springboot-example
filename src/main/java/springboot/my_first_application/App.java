c2dfd6e15b884ede9446b87c2d1f6f5100f0fa2d
Manikonda <Manikonda@Manikonda-PC.attlocal.net>
2018-09-11 20:06:39 -0500

    changes

--- src/main/java/springboot/my_first_application/App.java
+++ src/main/java/springboot/my_first_application/App.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
+		System.out.println(" Hello this from App");
 		SpringApplication.run(App.class, args);
 	}
 }
